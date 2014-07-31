4ff66f620682146379ca8f3b35d94f891574b9e0
Kim Hesselholt <khreenberg@gmail.com>
2014-07-31 21:43:18 +0200

    Adds a very simple score manager.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class manages score logic and UI
+/// </summary>
+public class ScoreManager : MonoBehaviour 
+{
+	/// <summary>
+	/// The global score variable. It might be useful to limit the scope of
+	/// this later.
+	/// </summary>
+	public static int Score { get; set; }
+}
