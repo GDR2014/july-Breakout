1000eca05c51e3cee9092156d199559e10d53074
KHReenberg <khreenberg@gmail.com>
2014-08-04 19:52:29 +0200

    Adds a simple level generator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    [SerializeField] private GameObject myBrick;
+
+    [SerializeField] private int myBrickWidth = 2, myBrickHeight = 1;
+
+    [SerializeField] private int myGridWidth = 16, myGridHeight = 8;
+
+    private float myGridMargin = .05f;
+    private float myCellSize;
+
+    void Start() {
+        Camera cam = Camera.main;
+        myCellSize = (cam.orthographicSize * cam.aspect * 2 - myGridMargin * 2) / myGridWidth;
+        FillGrid();
+    }
+
+    void FillGrid() {
+        for( int row = 0; row <= myGridHeight - myBrickHeight; row += myBrickHeight ) {
+            for( int col = 0; col <= myGridWidth - myBrickWidth; col += myBrickWidth ) {
+                Vector2 position = new Vector2();
+                position.x = col * myCellSize;
+                position.y = row * myCellSize;
+                GameObject brick = (GameObject) Instantiate( myBrick );
+                brick.transform.parent = transform;
+                brick.transform.localPosition = position;
+            }
+        }
+    }
+    
+}
