e4818c98c3a2078be4d1feb40af274fa87525592
KHReenberg <khreenberg@gmail.com>
2014-08-04 19:51:27 +0200

    Adds clamping to the paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -13,19 +13,35 @@ public class Paddle : MonoBehaviour
 	[SerializeField]
 	private float myMoveSpeed = 10f;
 
+    private float myMaxAbsoluteXPosition { get { return myMainCamera.orthographicSize * myMainCamera.aspect - collider2D.bounds.size.x / 2; }} // TODO: Move screen width calculation to a proper class
+
+    private Camera myMainCamera;
+
+    void Start() {
+        myMainCamera = Camera.main;
+    }
+
 	void Update()
 	{
 		handleInput();
+        clampPaddlePosition();
 	}
+
+    private void clampPaddlePosition() 
+    {
+        if (transform.position.x < -myMaxAbsoluteXPosition) transform.position = new Vector3(-myMaxAbsoluteXPosition, transform.position.y, transform.position.z);
+        else if (transform.position.x > myMaxAbsoluteXPosition) transform.position = new Vector3(myMaxAbsoluteXPosition, transform.position.y, transform.position.z); 
+    }
 
 	/// <summary>
 	/// Handles the player input
 	/// </summary>
 	private void handleInput()
 	{
-		Vector2 position = transform.position;
-		float horizontalInput = Input.GetAxis("Horizontal");
-		position.x += horizontalInput * myMoveSpeed * Time.deltaTime;
-		transform.position = position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 position = transform.position;
+        position.x += horizontalInput * myMoveSpeed * Time.deltaTime;
+        transform.position = position;
 	}
 }
